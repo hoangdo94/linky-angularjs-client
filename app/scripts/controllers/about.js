d0487c5e5930486ddbc6c8c5badd690d0123c29a
Hoang Do <hoangdo.dh94@gmail.com>
2016-03-12 10:41:52 +0700

    init the project using yo

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name linkyApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the linkyApp
+ */
+angular.module('linkyApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
