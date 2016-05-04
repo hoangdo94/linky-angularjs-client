f568fe6b0a1ed07ab16b45a5f9f5cc8cc77b6269
trungpdm-shinway <trungpdm@shinway.vn>
2016-05-04 22:31:44 +0700

    Register Remake

--- app/scripts/controllers/register.js
+++ app/scripts/controllers/register.js
@@ -8,12 +8,42 @@
  * Controller of the linkyApp
  */
 angular.module('linkyApp')
-  .controller('RegisterCtrl', function ($rootScope, $scope, $location) {
-    $scope.signUpUser = function(data) {
-    	console.dir(data);
-    };
+    .controller('RegisterCtrl', function($rootScope, $scope, $location, usersService, notify) {
+        $scope.signUpUser = function(data) {
+            usersService.create(data, function(res) {
+                if (res.error) {
+                    var alert = '';
+                    if (res.error.errors.email) {
+                        alert += '\n' + res.error.errors.email[0];
+                    }
+                    if (res.error.errors.username) {
+                        alert += '\n' + res.error.errors.username[0];
+                    }
+                    if (res.error.errors.password) {
+                        alert += '\n' + res.error.errors.password[0];
+                    }
+                    if (res.error.errors.website) {
+                        alert += '\n' + res.error.errors.website[0];
+                    }
 
-    $scope.goToLogin = function() {
-    	$location.path('/login');
-    };
-  });
+                    notify({
+                        message: alert,
+                        duration: 5000,
+                        position: 'center'
+                    });
+                } else {
+                  var welcome = 'Welcome to Linky! Good luck, have fun!';
+                    notify({
+                        message: welcome,
+                        duration: 5000,
+                        position: 'center'
+                    });
+                    $location.path('/');
+                }
+            });
+        };
+
+        $scope.goToLogin = function() {
+            $location.path('/login');
+        };
+    });
