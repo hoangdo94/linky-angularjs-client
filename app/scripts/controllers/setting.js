9afaaf654eb7269bddd4336a1fd414e4954d8033
trungpdm-shinway <trungpdm@shinway.vn>
2016-05-07 19:36:28 +0700

    Fix update avatar

--- app/scripts/controllers/setting.js
+++ app/scripts/controllers/setting.js
@@ -175,6 +175,7 @@ angular.module('linkyApp')
                                     position: 'center'
                                 });
                                 $rootScope.currentUser = res.data;
+                                $scope.profileUser = $rootScope.currentUser;
                             } else {
                                 notify({
                                     message: 'Failed to change avatar. Please try again.',
